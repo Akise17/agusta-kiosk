88393229a3645a6a166f691d980b1ed6005a22b5
Akise17 <ahmad.fauzan1603@gmail.com>
2024-02-15 02:12:35 +0700

    add mqtt event

---
+++ mqtt_event.py
@@ -0,0 +1,55 @@
+import paho.mqtt.client as mqtt
+import serial
+import time
+
+# Define callback function to handle incoming messages
+class SerialCommand():
+    def send(command):
+        SERIAL_PORT = '/dev/ttyUSB0'
+        BAUD_RATE = 9600
+        
+        try:
+            ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
+            print("Serial port opened successfully.")
+            print(command)
+            time.sleep(1.5)
+            ser.write((command + '\n').encode())
+
+            response = ser.readline().decode().strip()
+            print("Response:", response)
+        
+        except Exception as e:
+            print("Error:", str(e))
+        finally:
+            if ser.is_open:
+                ser.close()
+                print("Serial port closed.")
+def on_message(client, userdata, message):
+    print("Received message '" + str(message.payload) + "' on topic '"
+          + message.topic + "' with QoS " + str(message.qos))
+    if message.topic == "topics/machine":
+            if str(message.payload) == "b'dryer_on'":
+                SerialCommand.send("run_dryer:60")
+
+# Set up MQTT client instance
+client = mqtt.Client()
+
+# Set up callback function
+client.on_message = on_message
+
+# Set username and password
+username = "agusta-dev"
+password = "12345678"
+client.username_pw_set(username, password)
+
+# Connect to MQTT broker
+broker_address = "agusta-dev.siap.tech"
+port = 1880
+client.connect(broker_address, port)
+
+# Subscribe to a topic
+topic = "topics/#"
+client.subscribe(topic)
+
+# Start the network loop to process incoming messages
+client.loop_forever()
